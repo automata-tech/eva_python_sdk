1122700b61b9a6f253ce5330b138a5a49e904ccf
charlesworth <ch.c@automata.tech>
2019-11-20 11:24:42 +0000

    fix the ws example code

--- examples/http_ws_example.py
+++ examples/http_ws_example.py
@@ -17,14 +17,18 @@ print('ip: [{}], token: [{}]\n'.format(host_ip, token))
 
 http_client = evasdk.EvaHTTPClient(host_ip, token)
 
+# The session token will be valid for 30 minutes, you'll need to renew the session
+# if you want the websocket connection to continue after that point.
+session_token = http_client.auth_create_session()
+
 users = http_client.users_get()
 print('Eva at {} users: {}\n'.format(host_ip, users))
 
 joint_angles = http_client.data_servo_positions()
 print('Eva current joint angles: {}'.format(joint_angles))
 
-async def eva_ws_example(host_ip, token):
-    websocket = await evasdk.ws_connect(host_ip, token)
+async def eva_ws_example(host_ip, session_token):
+    websocket = await evasdk.ws_connect(host_ip, session_token)
 
     msg_count = 0
     time_since_msg = time.time()
@@ -37,4 +41,4 @@ async def eva_ws_example(host_ip, token):
         ws_msg = json.loads(ws_msg_json)
         print(ws_msg)
 
-asyncio.get_event_loop().run_until_complete(eva_ws_example(host_ip, token))
+asyncio.get_event_loop().run_until_complete(eva_ws_example(host_ip, session_token))
