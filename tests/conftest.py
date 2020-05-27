980d7f57342892909cfe9ce176a8d081cd79cef3
charlesworth <ch.c@automata.tech>
2020-05-27 16:01:25 +0100

    added Eva_GPIO tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,12 @@ def pytest_addoption(parser):
     parser.addoption("--token", default='', help="API token of the test robot")
     parser.addoption("--runslow", action="store_true", default=False, help="run slow tests")
     parser.addoption("--runrobot", action="store_true", default=False, help="run tests that require a connected Eva")
+    parser.addoption(
+        "--io_loopback",
+        action="store_true",
+        default=False,
+        help="run tests that require head and base IO loopback cables connected to Eva",
+    )
 
 
 @pytest.fixture(scope="session")
@@ -23,26 +29,34 @@ def token(request):
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
     config.addinivalue_line("markers", "robot_required: no mocks, needs a connected Eva to run")
+    config.addinivalue_line(
+        "markers",
+        "io_loopback_required: require head and base IO loopback cables connected to Eva",
+    )
 
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
-    else:
+    # --runslow given in cli: do not skip slow tests
+    if not config.getoption("--runslow"):
         skip_slow = pytest.mark.skip(reason="need --runslow option to run")
         for item in items:
             if "slow" in item.keywords:
                 item.add_marker(skip_slow)
 
-    if config.getoption("--runrobot"):
-        # --runrobot given in cli: do not skip tests where a connected Eva is required
-        return
-    else:
+    # --runrobot given in cli: do not skip tests where a connected Eva is required
+    if not config.getoption("--runrobot"):
         skip_robot_required = pytest.mark.skip(reason="need --runrobot option to run")
         for item in items:
             if "robot_required" in item.keywords:
                 item.add_marker(skip_robot_required)
+
+    # --io_loopback given in cli: do not skip tests requiring
+    # attached base and ee IO loopback cables
+    if not config.getoption("--io_loopback"):
+        skip_io_loopback_required = pytest.mark.skip(reason="need --io_loopback option to run")
+        for item in items:
+            if "io_loopback_required" in item.keywords:
+                item.add_marker(skip_io_loopback_required)
 
 
 @pytest.fixture(scope="module")
