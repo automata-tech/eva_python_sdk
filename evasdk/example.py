2566f648a4f20430a44aa7e0d366ffa0f93abf31
Tom Allpress <tom@automata.tech>
2021-09-03 11:42:10 +0100

    test out if a new file can be added

---
+++ evasdk/example.py
@@ -0,0 +1,10 @@
+class Example:
+    """Example is to see if doc gen can pick up a new file.
+    """
+    def some_func(self, arg: str) -> None:
+        """Some func prints the arg.
+
+        Args:
+            arg (str): something to print
+        """
+        print(arg)
